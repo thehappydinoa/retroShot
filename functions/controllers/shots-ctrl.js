cac738ed14556dd21627cd2032ab7aafd36c6b12
Aidan Holland <thehappydinoa@gmail.com>
2020-11-10 12:54:12 -0500

    chore: init paths

---
+++ functions/controllers/shots-ctrl.js
@@ -0,0 +1,26 @@
+const functions = require('firebase-functions');
+
+createShot = (req, res) => {}
+
+createShots = (req, res) => {}
+
+updateShot = (req, res) => {}
+
+deleteShot = (req, res) => {}
+
+getShotById = (req, res) => {}
+
+getShots = async (req, res) => {}
+
+getRandomShot = async (req, res) => {}
+
+
+module.exports = {
+  createShot,
+  createShots,
+  updateShot,
+  deleteShot,
+  getShotById,
+  getShots,
+  getRandomShot
+}
---
+++ functions/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+
+const router = require("./routes");
+
+// const host = "localhost";
+// const apiPort = 5000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to retroShot API!");
+});
+
+app.use("/api", router);
+
+// app.listen(apiPort, () =>
+//   console.log(`Server running on port http://${host}:${apiPort}`)
+// );
+
+module.exports = {
+  app,
+};
