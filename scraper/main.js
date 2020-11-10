4dda7f5334f063a6c3e3a697eeba8e73bc8327fb
mmelkumyan <47286353+mmelkumyan@users.noreply.github.com>
2020-11-10 15:39:36 -0500

    adds basic info to json

--- scraper/main.js
+++ scraper/main.js
@@ -4,47 +4,60 @@ const subreddit = "oldschoolcool";
 const postsPerRequest = 1;
 
 async function fetchPosts(){
-//    await axios.get('https://www.reddit.com/r/oldschoolcool.json')
-//        .then(function (response) {
-//            return response.json();
-//        }).then(function (data) {
-//            console.log("client received from server:");
-//            console.log(data)
-//
-//    });
-    //const response = await
+
+    let responseShots = {"shots": []}
     
-    
-    //TODO: Make sure is_video=false, over_18=false
     axios.get(`https://www.reddit.com/r/${subreddit}.json?limit=${postsPerRequest}`).then(function (response) {
-        console.log(response.data)
         const posts = response.data.data.children
         const after = response.data.data.after
         
-        console.log(posts[0])
-        //res.json({"feelsLikeFarenheit": feelsLike});
-	});
+        posts.forEach(function(post){
+            let data = post.data;
+            
+            if (data.is_video || data.over_18){ //exclude video and NSFW posts
+                return;
+            }
+            
+            let shot = {
+                "postId": data.id,
+                "user": data.author,
+                "postUrl": `https://www.reddit.com${data.permalink}`,
+                "imgUrl": data.url,
+                "year": '',
+                "decade": '',
+            }
+            
+            //TODO: regex magic
+            //Criteria:
+            //- 4 digit number, with OR without 's' at end. ir ''s'
+            //-- if ends in 's', set as decade.
+            let title = data.title;
+            console.log(title)
+            
+            responseShots.shots.push(shot);
+            
+        })
+        
+        console.log("responseShots:");
+        //console.log(responseShots)
+        
+	}).catch(function (error) {
+        console.log(error)
+        return;
+    });
     
     
-    console.log("client received from server:");
+    
     
     
 }
-// date 
-// img
-// year
-// username
-// postID
-
-//let shots = {"shots": [{shot1...}, {shot2...}, {shot3...}]}
-
-let shot = {
-  postId: 123,
-  user: "user123",
-  url: "https://...", //img URL
-  year: 2019, //regex'd year
-  // If year is none decade will be used, if neither an error will be thrown
-  decade: "2010s",
-}
+//let shot = {
+//  postId: 123,
+//  user: "user123",
+//  url: "https://...", //img URL
+//  year: 2019, //regex'd year
+//  // If year is none decade will be used, if neither an error will be thrown
+//  decade: "2010s",
+//}
 
 fetchPosts()
