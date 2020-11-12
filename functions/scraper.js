cb315af8751ce6683d9f7483f40dafca6a2b7eeb
Aidan Holland <thehappydinoa@gmail.com>
2020-11-12 11:40:09 -0500

    feat: organize scraper

---
+++ functions/scraper.js
@@ -0,0 +1,58 @@
+let axios = require("axios");
+
+async function fetchShots(postsPerRequest = 100) {
+  let shots = [];
+
+  return axios
+    .get(`https://www.reddit.com/r/oldschoolcool.json?limit=${postsPerRequest}`)
+    .then((response) => {
+      const posts = response.data.data.children;
+      const after = response.data.data.after;
+
+      posts.forEach((post) => {
+        let data = post.data;
+
+        // Exclude video and NSFW posts
+        if (data.is_video || data.over_18) {
+          return;
+        }
+
+        // Match exact years or decades
+        let regex = /\d{4}([’']?s)?/g;
+        let title = data.title;
+        let matches = title.match(regex);
+
+        // Accept only a single match per title
+        if (!matches || matches.length > 1) {
+          return;
+        }
+
+        // Extract year/ decade int from match
+        let year = null;
+        let decade = null;
+        if (matches[0].slice(-1) === "s") {
+          decade = parseInt(matches[0].slice(0, 4), 10);
+        } else {
+          year = parseInt(matches[0], 10);
+        }
+
+        // Build shot JSON object and push to shots
+        let shot = {
+          postId: data.id,
+          user: data.author,
+          postUrl: `https://www.reddit.com${data.permalink}`,
+          imgUrl: data.url,
+          year: year,
+          decade: decade,
+        };
+        shots.push(shot);
+      });
+
+      return shots;
+    })
+    .catch(console.warn);
+}
+
+module.exports = {
+  fetchShots,
+};
