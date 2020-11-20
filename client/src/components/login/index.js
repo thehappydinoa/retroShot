064af02adf9b7582c85180a8ba7d3a1f645108ad
Aidan Holland <thehappydinoa@gmail.com>
2020-11-20 14:00:09 -0500

    chore: handle login

--- client/src/components/login/index.js
+++ client/src/components/login/index.js
@@ -34,24 +34,29 @@ const Login = ({ firebase }) => {
     // TODO: Catch already in use
   };
 
+  const handleSubmit = (event) => {
+    login();
+    // signup();
+  };
+
   return (
     <Form>
-        <Form.Group controlId="login" onSubmit={login}>
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
+      <Form.Group controlId="login" onSubmit={handleSubmit}>
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
 
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Login
-        </Button>
-      </Form>
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Login
+      </Button>
+    </Form>
   );
 };
 
