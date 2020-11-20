81cfc2473d83aa4385fd22ee8e26b191620de4c6
Aidan Holland <thehappydinoa@gmail.com>
2020-11-20 13:47:04 -0500

    feat: bootstrap

--- client/src/components/nav/index.js
+++ client/src/components/nav/index.js
@@ -1,22 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Navbar, Nav } from 'react-bootstrap';
 
 import { withFirebase } from "../firebase";
 import * as ROUTES from "../../routes";
 
-const Nav = ({ firebase }) => {
+const NavBar = ({ firebase }) => {
   return (
-    <div>
-      <ul>
-        <li>
-          <Link to={ROUTES.LANDING}>Home</Link>
-        </li>
-        <li>
-          <Link to={ROUTES.LOGIN}>Login</Link>
-        </li>
-      </ul>
-    </div>
+    <Navbar bg="light" variant="light">
+    <Navbar.Brand href={ROUTES.LANDING}>retroShot</Navbar.Brand>
+    <Nav>
+      <Nav.Link href={ROUTES.LANDING}>Home</Nav.Link>
+      <Nav.Link href={ROUTES.LOGIN}>Login</Nav.Link>
+    </Nav>
+  </Navbar>
   );
 };
 
-export default withFirebase(Nav);
+export default withFirebase(NavBar);
