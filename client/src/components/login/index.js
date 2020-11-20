08a4d221ee6b9a2242bd800682498fdadc599bde
Aidan Holland <thehappydinoa@gmail.com>
2020-11-20 13:35:04 -0500

    feat: signin

---
+++ client/src/components/login/index.js
@@ -0,0 +1,42 @@
+import React, { useRef } from "react";
+import { useHistory } from "react-router-dom";
+
+import { withFirebase } from "../firebase";
+import * as ROUTES from "../../routes";
+
+const Login = ({ firebase }) => {
+  let history = useHistory();
+
+  const userRef = useRef(null);
+  const passRef = useRef(null);
+
+  const login = () => {
+    firebase
+      .signInWithEmailAndPassword(userRef.current.value, passRef.current.value)
+      .then((user) => {
+        console.log(user);
+        history.push(ROUTES.LANDING);
+      });
+    // TODO: Catch already in use
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <label>
+          Username:
+          <input type="text" ref={userRef} />
+        </label>
+
+        <label>
+          Password:
+          <input type="text" ref={passRef} />
+        </label>
+      </form>
+      <button onClick={login}>Login</button>
+    </div>
+  );
+};
+
+export default withFirebase(Login);
--- client/src/components/nav/index.js
+++ client/src/components/nav/index.js
@@ -1,19 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { withFirebase } from "../firebase";
 import * as ROUTES from "../../routes";
 
-const Nav = () => (
-  <div>
-    <ul>
-      <li>
-        <Link to={ROUTES.LANDING}>Landing</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.LOGIN}>Log In</Link>
-      </li>
-    </ul>
-  </div>
-);
+const Nav = ({ firebase }) => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to={ROUTES.LANDING}>Home</Link>
+        </li>
+        <li>
+          <Link to={ROUTES.LOGIN}>Login</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default Nav;
+export default withFirebase(Nav);
