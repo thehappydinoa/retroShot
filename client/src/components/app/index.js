817d36a51999f05d07319a0dba58a867fcc20821
Aidan Holland <thehappydinoa@gmail.com>
2020-11-20 13:35:04 -0500

    feat: signin

--- client/src/components/app/index.js
+++ client/src/components/app/index.js
@@ -1,11 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Nav from "../nav";
+import Home from "../home";
+import Login from "../login";
+
+import * as ROUTES from "../../routes";
 
 const App = () => (
   <Router>
     <Nav />
+    <Route exact path={ROUTES.LANDING}>
+      <Home />
+    </Route>
+    <Route path={ROUTES.LOGIN}>
+      <Login />
+    </Route>
   </Router>
 );
 
