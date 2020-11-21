ffb4492188dae65ef081e0bc67c9c70887cf98cc
mmelkumyan <47286353+mmelkumyan@users.noreply.github.com>
2020-11-20 23:20:53 -0500

    adds answer component and minor styling

--- client/src/components/title/index.js
+++ client/src/components/title/index.js
@@ -4,8 +4,8 @@ import "./title.css";
 
 const Title = () => (
   <div className='title-container'>
-    <h1>With pictures taken from www.reddit.com/r/OldSchoolCool/
-Can you guess what year this picture was taken?</h1>
+    <h1>With pictures taken from www.reddit.com/r/OldSchoolCool/</h1>
+    <h1>Can you guess what year this picture was taken?</h1>
   </div>
 );
 
