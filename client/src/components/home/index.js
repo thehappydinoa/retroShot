bacc11251900fa84bbd3e630d8a5d964e6e784cc
mmelkumyan <47286353+mmelkumyan@users.noreply.github.com>
2020-11-20 21:24:51 -0500

    creates react component outlines and fetch in api shots

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 
+import Title from "../title";
+import Shot from "../shot";
+import Footer from "../footer";
 import { withFirebase } from "../firebase";
 
+import "./home.css";
+
 const Home = ({ firebase }) => (
-  <div>
+  <div className='home-container'>
     <h1>Home</h1>
+    <Title></Title>
+    <Shot></Shot>
     {firebase.auth.currentUser?.email || "User Not Signed In"}
+    <Footer></Footer>
   </div>
 );
 
