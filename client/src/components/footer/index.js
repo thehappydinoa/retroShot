91e01657842d76e4b74aa57a525ccbdb73fd6c3a
mmelkumyan <47286353+mmelkumyan@users.noreply.github.com>
2020-11-20 21:26:20 -0500

    create react component outlines

---
+++ client/src/components/footer/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer = () => (
+  <div>
+    CS375 Final Project
+    Created by: Aidan Holland, Hoang Nguyen, Mark Melkumyan, Likitha Reddy Pailla,  Han Cao
+  </div>
+);
+
+export default Footer;
