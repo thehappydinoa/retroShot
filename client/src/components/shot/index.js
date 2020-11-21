3222af54b6b560c78c072696285b142b56cf0686
mmelkumyan <47286353+mmelkumyan@users.noreply.github.com>
2020-11-20 21:26:20 -0500

    create react component outlines

---
+++ client/src/components/shot/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+import "./shot.css";
+
+// const Shot = () => {
+//   let i = 0;
+//   return (
+//     <div className='shot-container'>
+//         <p>Insert shot here</p>
+//     </div>
+//   );
+// };
+
+class FetchRandomShot extends React.Component {
+
+  state = {
+    isLoading: true,
+    shot: null,
+  }
+
+  async componentDidMount() {
+    const url = "https://us-central1-***REMOVED***.cloudfunctions.net/api/shot/random"
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log("DATA")
+    console.log(data)
+    this.setState({
+      isLoading: false,
+      shot: data,
+    });
+  } 
+
+  render() {
+    
+    let {isLoading, shot} = this.state;
+
+    return ( 
+      <div className='shot-container'>
+        {isLoading || !shot ? (
+          <div>loading...</div>
+        ) : (
+          <img className='shot-img' src={shot.data.imgUrl} alt="Retro shot"/> 
+        )}
+      </div>
+    )
+  }
+}
+
+export default FetchRandomShot;
---
+++ client/src/components/title/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./title.css";
+
+const Title = () => (
+  <div className='title-container'>
+    <h1>With pictures taken from www.reddit.com/r/OldSchoolCool/
+Can you guess what year this picture was taken?</h1>
+  </div>
+);
+
+export default Title;
