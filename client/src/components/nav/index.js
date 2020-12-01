5549fb0d73e70825f1738de16f32dc7afd9ea3ae
Aidan Holland <thehappydinoa@gmail.com>
2020-12-01 03:02:40 -0500

    feat: favicon

--- client/src/components/nav/index.js
+++ client/src/components/nav/index.js
@@ -6,9 +6,17 @@ import { withFirebase } from "../firebase";
 const NavBar = ({ firebase }) => {
   return (
     <Navbar bg="light" variant="light">
-      <Navbar.Brand href={ROUTES.LANDING}>retroShot</Navbar.Brand>
+      <Navbar.Brand href={ROUTES.LANDING}>
+        <img
+          src="/camera.png"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+          alt="retroShot Logo"
+        />{" "}
+        retroShot
+      </Navbar.Brand>
       <Nav>
-        <Nav.Link href={ROUTES.LANDING}>Home</Nav.Link>
         <Nav.Link href={ROUTES.LANDING}>New Game</Nav.Link>
         {firebase.auth.currentUser ? (
           <Nav.Link disabled>{firebase.auth.currentUser.email}</Nav.Link>
