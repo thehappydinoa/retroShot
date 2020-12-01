7a24a0718d389b68c2254b09ab80e35207e08517
Aidan Holland <thehappydinoa@gmail.com>
2020-12-01 11:57:37 -0500

    feat: font

--- client/src/components/app/index.js
+++ client/src/components/app/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./app.css";
 
 import Nav from "../nav";
 import Footer from "../footer";
@@ -10,16 +11,18 @@ import Login from "../login";
 import * as ROUTES from "../../routes";
 
 const App = () => (
-  <Router>
-    <Nav />
-    <Route exact path={ROUTES.LANDING}>
-      <Home />
-    </Route>
-    <Route path={ROUTES.LOGIN}>
-      <Login />
-    </Route>
-    <Footer/>
-  </Router>
+  <div className="app">
+    <Router>
+      <Nav />
+      <Route exact path={ROUTES.LANDING}>
+        <Home />
+      </Route>
+      <Route path={ROUTES.LOGIN}>
+        <Login />
+      </Route>
+      <Footer />
+    </Router>
+  </div>
 );
 
 export default App;
