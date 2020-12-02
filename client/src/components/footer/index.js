7461a488a97bd4ec39275e07db1c18897f3552c2
JJJJHan <53534957+JJJJHan@users.noreply.github.com>
2020-12-02 19:48:47 +0800

    layout changes

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Navbar } from "react-bootstrap";
-
+import "./footer.css";
 const Footer = () => (
-  <Navbar fixed="bottom">
-    CS375 Final Project Created by: Aidan Holland, Han Cao, Hoang Nguyen,
+  <Navbar fixed="bottom" class='footer'>
+    CS375 Final Project retroShot<br />
+    Created by: Aidan Holland, Han Cao, Hoang Nguyen,
     Likitha Reddy Pailla, and Mark Melkumyan.
   </Navbar>
 );
