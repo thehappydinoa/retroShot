8f91bf89e1d3031444081a95a4c0e951b78fde4f
Aidan Holland <thehappydinoa@gmail.com>
2020-12-10 21:23:53 -0500

    feat: type of random shot

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,7 +1,20 @@
-const BASE_URL = "https://us-central1-retroshot-6a964.cloudfunctions.net/api";
+const URLS = {
+  dev: "http://localhost:5001/retroshot-6a964/us-central1/api",
+  prod: "https://us-central1-retroshot-6a964.cloudfunctions.net/api",
+};
 
-const getRandomShot = () => {
-  return fetch(`${BASE_URL}/shot/random`);
+// Set true for dev
+const dev = false;
+
+const BASE_URL = dev ? URLS.dev : URLS.prod;
+
+const getRandomShot = (type = "year") => {
+  let url = `${BASE_URL}/shot/random`;
+  if (type) {
+    url += `?${new URLSearchParams({ type })}`;
+  }
+  console.log(url);
+  return fetch(url);
 };
 
 export { BASE_URL, getRandomShot };
